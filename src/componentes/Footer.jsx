9025f553760ac507d99835414f146ecc3ef1e4db
Pedro Chacón <pedrochacon9621@gmail.com>
2025-10-02 21:08:55 +0200

    edit 02102025 04

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -2,7 +2,7 @@ export function Footer() {
     return(
         <div>
             <div className="ancho-max">
-                <footer className="footer1">
+                <footer className="footer1" style={{display:"none"}}>
                 </footer>
             </div>
         </div>
