57920a9499553cb028005aff5ba045dcae9e7388
Pedro Chacón <pedrochacon9621@gmail.com>
2025-10-02 21:28:44 +0200

    edit 02102025 05

--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,12 +1,35 @@
-import { FormLogin } from '../componentes/FormLogin'
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../context/UserContext";
+import { FormLogin } from "../componentes/FormLogin";
+
 export function Inicio() {
-    return(
+    const { user, loading } = useUser();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!loading && user) {
+            navigate("/productos");
+        }
+    }, [loading, user, navigate]);
+
+    if (loading) {
+        return (
+            <div className="modal-loading">
+                <div className="modal-content">
+                    <p>Verificando sesión...</p>
+                </div>
+            </div>
+        );
+    }
+
+    return (
         <div>
             <div className='mainL'>
                 <div>
-                    <FormLogin/>
+                    <FormLogin />
                 </div>
             </div>
         </div>
-    )
+    );
 }
